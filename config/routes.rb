970aa3ad61e8f6979b3e6ed30ebe4b6453bb696a
annemnichols <anne@seantnichols.com>
2015-01-20 12:17:14 -0700

    Set up Tweet model and associations with User

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'users/show'
-
-  get 'users/index'
-
   root 'welcome#index'
 
   devise_for :users, :controllers => { registrations: 'registrations' }
+
+  resources :users, only: [:show, :index] do
+    resources :tweets
+  end
 
 end
