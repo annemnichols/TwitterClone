5ab8a40cf5c4f7c4d85ab125e2db8b637ef18063
annemnichols <anne@seantnichols.com>
2015-01-23 11:41:34 -0700

    Initiated OmniAuth to allow users to sign up with their Twitter account

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   root 'tweets#index'
 
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, :controllers => { registrations: 'registrations', 
+  																		 :omniauth_callbacks => "users/omniauth_callbacks" }
 
   resources :users, only: [:show, :index] do
     resources :tweets, except: [:index]
